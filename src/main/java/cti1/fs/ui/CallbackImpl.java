c2aab33c28a3c8fd0e772479908323a058324c40
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-06 16:01:32 +0100

    Rozroznienie typu operacji w callbacku

--- src/main/java/cti1/fs/ui/CallbackImpl.java
+++ src/main/java/cti1/fs/ui/CallbackImpl.java
@@ -2,6 +2,7 @@ package cti1.fs.ui;
 
 import cti1.fs.ICallback;
 import cti1.fs.ITaskStatus;
+import cti1.fs.OpType;
 
 /**
  * Created by Michał on 2016-11-06.
@@ -9,6 +10,14 @@ import cti1.fs.ITaskStatus;
 public class CallbackImpl implements ICallback {
     @Override
     public void finished(ITaskStatus taskStatus) {
-        System.out.print("finished!");
+
+        if (taskStatus.getType().equals(OpType.Read)) {
+            System.out.print("finished!");
+        } else if (taskStatus.getType().equals(OpType.Write)) {
+            System.out.print("finished!");
+        } else {
+            throw new IllegalArgumentException("Nieobslugiwany typ operacji");
+        }
+
     }
 }
